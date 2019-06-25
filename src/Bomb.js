52897b929bac8ecfffe36c1d23d800693a61fbdc
LeahB8 <leahboyd08@gmail.com>
2019-06-25 10:50:19 +0100

    Done

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            secondsLeft: this.props.initialCount
+        }
+    }
+
+    // countdownTimer = () => {
+    //     let newValue = this.props.initialCount --
+    //     this.setState({
+    //         secondsLeft: newValue
+    //     })
+    // }
+
+    render() {
+        return (
+            <div>
+                {this.state.secondsLeft === 0 ? <p>Boom!</p> : <p>{this.state.secondsLeft} seconds left before I go boom!</p>}
+            </div>
+        )
+    }
+}
